4e436aa03e6cd4c18bb78040178129d32d22386b
KatoMono Enkeli <k4t0mono@gmail.com>
2018-12-29 23:08:15 -0200

    Pre Deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,51 @@
-// webpack config
-
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-	entry: './src/app.jsx',
-	output: {
-		path: path.resolve(__dirname, './public/scripts'),
-		filename: 'bundle.js'
-	},
-	mode: 'development',
-	module: {
-		rules: [{
-			loader: 'babel-loader',
-			test: /\.jsx?$/,
-			exclude: /node_modules/
-		}, {
-			test: /\.s?css$/,
-			use: ['style-loader', 'css-loader', 'sass-loader']
-		}]
-	},
-	devtool: 'cheap-module-eval-source-map',
-	devServer: {
-		contentBase: path.resolve(__dirname, './public'),
-		historyApiFallback: true
-	}
+
+
+module.exports = (env) => {
+	const isProd = env === 'prod';
+	const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
+
+	return {
+		entry: './src/app.jsx',
+		output: {
+			path: path.resolve(__dirname, 'public'),
+			filename: 'bundle.js'
+		},
+		mode: 'development',
+		module: {
+			rules: [
+				{
+					loader: 'babel-loader',
+					test: /\.jsx?$/,
+					exclude: /node_modules/
+				},
+				{
+				 test: /\.s?css$/,
+				 use: [
+					 MiniCssExtractPlugin.loader,
+					 {
+						 loader: 'css-loader',
+						 options: {
+							 sourceMap: true
+						 }
+					 },
+					 {
+						 loader: 'sass-loader',
+						 options: {
+							 sourceMap: true
+						 }
+					 }
+				 ]
+				}
+			]
+		},
+		plugins: [ CSSExtract ],
+		devtool: isProd ? 'source-map1' : 'inline-source-map',
+		devServer: {
+			contentBase: path.resolve(__dirname, './public/'),
+			historyApiFallback: true
+		}
+	};
 };
